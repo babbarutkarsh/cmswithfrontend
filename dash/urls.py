97b195936bcda27a777edee61db8204d9e10eae1
babbarutkarsh <46282290+babbarutkarsh@users.noreply.github.com>
2020-11-21 21:39:04 +0530

    Second commit

--- dash/urls.py
+++ dash/urls.py
@@ -5,8 +5,8 @@ from .import views
 urlpatterns = [
     path('',views.index,name='Home'),
     path('about/',views.about,name='About'),
-    path('registration/',views.showformdata),
-    path('status/',views.reqInfo),
+    path('registration/',views.showformdata,name='Form'),
+    path('status/',views.reqInfo,name='Status'),
     path('student_approve/<str:user_id>', views.student_approve,name="student_approve"),
     path('student_disapprove/<str:user_id>', views.student_disapprove,name="student_disapprove"),
 ]
